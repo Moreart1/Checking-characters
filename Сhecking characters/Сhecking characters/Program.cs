f3c166243f65210ebcd53c4b0c0ee24ca84a5ce7
Moreart1 <morearti1726263@yandex.ru>
2022-05-06 16:51:59 +0300

    Проверка  расстановки скобок

--- Сhecking characters/Сhecking characters/Program.cs
+++ Сhecking characters/Сhecking characters/Program.cs
@@ -1,1 +1,28 @@
 ﻿
+class Programm
+{
+    public static bool Check(string text)
+    {
+        int count = 0;
+        char[] storage = new char[100];
+        foreach (char element in text)
+        {
+            switch (element)
+            {
+                case '{': storage[++count] = element; break;
+                case '[': storage[++count] = element; break;
+                case '(': storage[++count] = element; break;
+                case '}': if (storage[count] == '{') { count--; } else { return false; }; break;
+                case ']': if (storage[count] == '[') { count--; } else { return false; }; break;
+                case ')': if (storage[count] == '(') { count--; } else { return false; }; break;
+            }
+        }
+        return count == 0 ? true : false;
+    }
+
+    static void Main(string [] args)
+    {
+        Console.WriteLine(Check(Console.ReadLine()));
+        Console.ReadKey(true);
+    }
+}
